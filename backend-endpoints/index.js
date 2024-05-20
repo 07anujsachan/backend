4bfb8a466ce52fc9632b7aef20744ecb3101890e
Anuj Sachan <anujsachan98@gmail.com>
2024-05-21 00:15:06 +0530

    updates

--- backend-endpoints/index.js
+++ backend-endpoints/index.js
@@ -1,33 +1,25 @@
-// index.js
+const express = require("express");
+const cors = require("cors");
+const { exec } = require("child_process");
+const { error } = require("console");
 
-const { createServer } = require('http');
-const { parse } = require('url');
-const next = require('next');
-const { exec } = require('child_process');
+const app = express();
+app.use(cors());
+const PORT = 4000;
+app.post("/test-speed", (req, res) => {
+  try {
+    exec(`fast --upload --json`, (err, stdout, stderr) => {
+      if (err || stderr) {
+        return res.status(400).json({ error: err, code: 400 });
+      }
+      const result = JSON.parse(stdout);
+      res.status(200).json({ code: 200, data: result });
+    });
+  } catch (error) {
+    res.status(500);
+  }
+});
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    if (pathname === '/speed' && req.method === 'POST') {
-      exec(`fast --upload --json`, (err, stdout, stderr) => {
-        if (err || stderr) {
-          return res.status(400).json({ error: err, code: 400 });
-        }
-        const result = JSON.parse(stdout);
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json({ code: 200, data: result });
-      });
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  });
+app.listen(PORT, () => {
+  console.log("Server Running ON port : ", PORT);
 });
